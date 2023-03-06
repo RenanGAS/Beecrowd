3269f4e9cb7246dafcd2b6abbe3712ce22273a20
RenanGAS <renansakaoki@gmail.com>
2023-03-06 12:13:45 -0300

    feat: add the banknotes and coins problem

--- 03-2023/minimumNotesCoins.py
+++ 03-2023/minimumNotesCoins.py
@@ -1,46 +1,21 @@
-import math
-
-# Usar vetor de notas, mudar quantia para usá-la em moedas (tem 1 real)
-
-def calcular_quantidade_notas(quantia, nota_inicial):
-    if int(float(nota_inicial)) == 1:
-        return
-    quantidade_notas = int(quantia) // int(float(nota_inicial))
-    print(str(quantidade_notas) + " notas(s) de R$ " + str(nota_inicial))
-    
-    quantia = quantia - (quantidade_notas * float(nota_inicial))
-    nota_inicial = int(float(nota_inicial)) // 2
-    nota_inicial = "{:.2f}".format(nota_inicial)
-    
-    calcular_quantidade_notas(quantia, nota_inicial)
-    
-def calcular_quantidade_moedas(quantia, lista_moedas, index_moeda):
-    if len(lista_moedas) == index_moeda:
-        return
-    
-    quantidade_moedas = 0
-    
-    while((quantia - lista_moedas[index_moeda]) >= 0):
-        quantidade_moedas += 1
-        quantia = quantia - lista_moedas[index_moeda]
-    
-    print(str(quantidade_moedas) + " moeda(s) de R$ " + str(lista_moedas[index_moeda]))
-    
-    index_moeda += 1
-    
-    calcular_quantidade_moedas(quantia, lista_moedas, index_moeda)
-
 def main():
-    quantia = float("{:.2f}".format(float(input())))
-    nota_inicial = "{:.2f}".format(float(100))
+    quantia = float(input())
+    lista_notas = [100, 50, 20, 10, 5, 2]
     lista_moedas = [1, 0.5, 0.25, 0.1, 0.05, 0.01]
-    index_moeda = 0
-    parte_decimal = quantia % 1
-    
+
     print("NOTAS:")
-    calcular_quantidade_notas(quantia, nota_inicial)
+    for nota in lista_notas:
+        quantidade_notas = int(quantia) // nota
+        print(str(quantidade_notas) + " nota(s) de R$ " + "{:.2f}".format(float(nota)))
+        quantia = quantia - (quantidade_notas * nota)
+
     print("MOEDAS:")
-    calcular_quantidade_moedas(parte_decimal, lista_moedas, index_moeda)
+    for moeda in lista_moedas:
+        quantidade_moedas = 0
+        while((round(quantia, 2) - moeda) >= 0):
+            quantidade_moedas += 1
+            quantia = quantia - moeda
+        print(str(quantidade_moedas) + " moeda(s) de R$ " + "{:.2f}".format(float(moeda)))
 
 if __name__ == "__main__":
     main()
