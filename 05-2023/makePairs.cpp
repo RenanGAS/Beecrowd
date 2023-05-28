32598773d378d40068cbcba7a0e0863c249d9d85
RenanGAS <renansakaoki@gmail.com>
2023-05-28 11:56:07 -0300

    feat: apparently solves the problem but got TLE

--- 05-2023/makePairs.cpp
+++ 05-2023/makePairs.cpp
@@ -1,64 +1,74 @@
 #include <iostream>
 #include <unordered_map>
+#include <map>
+#include <vector>
 #include <fstream>
 
 using namespace std;
 
-void adicionarElemento(unordered_map<int, int> &conjuntos_disjuntos, int aluno_x, int aluno_y)
+void mostrarGruposDisjuntos(map<int, unordered_map<int, int>> &grupos_disjuntos)
 {
-    if (conjuntos_disjuntos.at(aluno_x) == -1 && conjuntos_disjuntos.at(aluno_y) == -1)
+    cout << "\n";
+
+    for (auto it = grupos_disjuntos.cbegin(); it != grupos_disjuntos.cend(); ++it)
     {
-        conjuntos_disjuntos.at(aluno_x) = aluno_x;
-        conjuntos_disjuntos.at(aluno_y) = aluno_x;
-    }
-    else
-    {
-        if (conjuntos_disjuntos.at(aluno_x) == -1 && conjuntos_disjuntos.at(aluno_y) != -1)
+        cout << it->first << " -> ";
+        for (auto it1 = it->second.cbegin(); it1 != it->second.cend(); ++it1)
         {
-            conjuntos_disjuntos.at(aluno_x) = conjuntos_disjuntos.at(aluno_y);
+            cout << "(" << it1->first << ", " << it1->second << ") -> ";
         }
-        else if (conjuntos_disjuntos.at(aluno_x) != -1 && conjuntos_disjuntos.at(aluno_y) == -1)
+
+        cout << "\n";
+    }
+}
+
+void adicionarElemento(map<int, unordered_map<int, int>> &grupos_disjuntos, vector<int> &tamanho_grupos, int aluno_x, int aluno_y)
+{
+    int grupo_x = grupos_disjuntos.at(aluno_x).at(aluno_x);
+    int grupo_y = grupos_disjuntos.at(aluno_y).at(aluno_y);
+
+    if (grupo_x != grupo_y)
+    {
+        if (tamanho_grupos.at(grupo_y) == 1)
         {
-            conjuntos_disjuntos.at(aluno_y) = conjuntos_disjuntos.at(aluno_x);
+            grupos_disjuntos.at(grupo_y).at(grupo_y) = grupo_x;
+            grupos_disjuntos.at(grupo_x).emplace(grupo_y, grupo_x);
+            tamanho_grupos.at(grupo_x) += 1;
+            return;
+        }
+        else if (tamanho_grupos.at(grupo_x) == 1)
+        {
+            grupos_disjuntos.at(grupo_x).at(grupo_x) = grupo_y;
+            grupos_disjuntos.at(grupo_y).emplace(grupo_x, grupo_y);
+            tamanho_grupos.at(grupo_y) += 1;
+            return;
+        }
+
+        int novo_grupo, antigo_grupo = 0;
+
+        if (tamanho_grupos.at(grupo_x) >= tamanho_grupos.at(grupo_y))
+        {
+            antigo_grupo = grupo_y;
+            novo_grupo = grupo_x;
         }
         else
         {
-            int prox_pai_y = conjuntos_disjuntos.at(aluno_y);
+            antigo_grupo = grupo_x;
+            novo_grupo = grupo_y;
+        }
 
-            while (prox_pai_y != conjuntos_disjuntos.at(prox_pai_y))
-            {
-                prox_pai_y = conjuntos_disjuntos.at(prox_pai_y);
-                conjuntos_disjuntos.at(prox_pai_y) = conjuntos_disjuntos.at(aluno_x);
-            }
-
-            conjuntos_disjuntos.at(aluno_y) = conjuntos_disjuntos.at(aluno_x);
+        for (auto it = grupos_disjuntos.at(antigo_grupo).begin(); it != grupos_disjuntos.at(antigo_grupo).end(); ++it)
+        {
+            grupos_disjuntos.at(it->first).at(it->first) = novo_grupo;
+            it->second = novo_grupo;
+            tamanho_grupos.at(novo_grupo) += 1;
         }
     }
 }
 
-void verificarPai(unordered_map<int, int> &conjuntos_disjuntos, int aluno_x, int aluno_y)
+void verificarPai(map<int, unordered_map<int, int>> &grupos_disjuntos, int aluno_x, int aluno_y)
 {
-    if (conjuntos_disjuntos.at(aluno_x) == -1 || conjuntos_disjuntos.at(aluno_y) == -1)
-    {
-        cout << "N\n";
-        return;
-    }
-
-    // int pai_x = aluno_x;
-
-    // while (conjuntos_disjuntos.at(pai_x) != pai_x)
-    // {
-    //     pai_x = conjuntos_disjuntos.at(pai_x);
-    // }
-
-    // int pai_y = aluno_y;
-
-    // while (conjuntos_disjuntos.at(pai_y) != pai_y)
-    // {
-    //     pai_y = conjuntos_disjuntos.at(pai_y);
-    // }
-
-    if (conjuntos_disjuntos.at(aluno_x) == conjuntos_disjuntos.at(aluno_y))
+    if (grupos_disjuntos.at(aluno_x).at(aluno_x) == grupos_disjuntos.at(aluno_y).at(aluno_y))
     {
         cout << "S\n";
     }
@@ -77,12 +87,15 @@ int main()
 
     while (infile >> numero_alunos >> numero_amizades_diretas >> numero_pares)
     {
-        cout << "\n";
-        unordered_map<int, int> conjuntos_disjuntos;
+        // cout << "\n";
+        map<int, unordered_map<int, int>> grupos_disjuntos;
+        vector<int> tamanho_grupos(numero_alunos, 1);
 
         for (int i = 0; i < numero_alunos; i++)
         {
-            conjuntos_disjuntos.emplace(i, -1);
+            unordered_map<int, int> elementos;
+            elementos.emplace(i, i);
+            grupos_disjuntos.emplace(i, elementos);
         }
 
         for (int i = 0; i < numero_amizades_diretas; i++)
@@ -91,8 +104,10 @@ int main()
 
             infile >> aluno_x >> aluno_y;
 
-            adicionarElemento(conjuntos_disjuntos, aluno_x - 1, aluno_y - 1);
+            adicionarElemento(grupos_disjuntos, tamanho_grupos, aluno_x - 1, aluno_y - 1);
         }
+
+        // mostrarGruposDisjuntos(grupos_disjuntos);
 
         for (int i = 0; i < numero_pares; i++)
         {
@@ -100,7 +115,7 @@ int main()
 
             infile >> aluno_x >> aluno_y;
 
-            verificarPai(conjuntos_disjuntos, aluno_x - 1, aluno_y - 1);
+            verificarPai(grupos_disjuntos, aluno_x - 1, aluno_y - 1);
         }
     }
 
